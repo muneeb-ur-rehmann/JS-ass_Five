0f53ef0051694568b24a8fd3c83ff63a5f976f07
Muneeb <143552112+muneeb-ur-rehmann@users.noreply.github.com>
2024-04-01 10:09:54 -0700

    Add files via upload

---
+++ JS_ass_Five/app.js
@@ -0,0 +1,76 @@
+// Question One
+    // var a = [[]];
+
+// Question Two
+    // var mArr = [[0,1,2,3],
+    //             [1,0,1,2],
+    //             [2,1,0,1]
+    // ]
+    // for(a=0; a<4; a++){
+    //     for(b=0; b<4; b++){
+    //         document.write(mArr[a][b] + " ")
+    //     }
+    //     document.write("<br>")
+    // }
+
+// Question Three
+    // for(a=1; a<11; a++){
+    //     document.write(a + "<br>")
+    // }
+
+// Question Four
+    // var table = prompt("Enter table number:")
+    // var length = prompt("Define length:")
+
+    // document.write("Enter table number: " + table + "<br>")
+    // document.write("length you define " + length + "<br>");
+    // for(a=1; a<=length; a++){
+    //     document.write(table + "x" + a + "=" + table*a + "<br>");
+    // }
+
+// Question Five
+    // var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+    // document.write(fruits);
+
+// Question Six
+// (Counting)
+    // for(a=1; a<=15; a++){
+    //     document.write(a + "<br>")
+    // }
+
+// (Reverse Counting)
+    // var i = 15;
+    // for(a=i; a>0; a--){
+    //     document.write(a + " ")
+    // }
+
+// (Even Numers)
+    // var b = 100;
+    // for(a=2; a<=b; a = a+2){
+    //     document.write(a + " ")
+    // }
+
+// (Odd Numbers)
+    // var b = 100;
+    // for(a=1; a<=b; a = a+2){
+    //     document.write(a + " ")
+    // }
+
+// (Series Numbers)
+    // var b = 20;
+    // for(a=2; a<=b; a=a+2){
+    //     document.write(a + "K" + " ")
+    // }
+
+// Question Eight
+    // var arr = [24,53,72,91,12]
+    // for(a=0; a<=arr.length; a++){
+    // // if(arr[a]>arr[0]){
+    // //         document.write(arr[a])
+    // //     } else 
+    // if (arr[a+1]>arr[0]){
+    //         document.write(arr[a])
+    //     }
+        
+    // }
